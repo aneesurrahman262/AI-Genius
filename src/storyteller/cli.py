c25e87d6c69a37ae8a7c98ad6edf4c5269a0e361
aneesurrahman262 <132897577+aneesurrahman262@users.noreply.github.com>
2023-05-14 22:36:47 +0500

    Update cli.py

--- src/storyteller/cli.py
+++ src/storyteller/cli.py
@@ -13,13 +13,32 @@ def get_args() -> argparse.Namespace:
         "--writer_prompt",
         type=str,
         default="Once upon a time, unicorns roamed the Earth.",
+        help="The prompt for the writer model.",
     )
     parser.add_argument(
-        "--painter_prompt_prefix", type=str, default="Beautiful painting"
+        "--painter_prompt_prefix",
+        type=str,
+        default="Beautiful painting",
+        help="The prefix for the painter prompt.",
     )
-    parser.add_argument("--num_images", type=int, default=10)
-    parser.add_argument("--output_dir", type=str, default="out")
-    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument(
+        "--num_images",
+        type=int,
+        default=10,
+        help="The number of images to generate.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="out",
+        help="The output directory to save the generated content.",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42,
+        help="The random seed for reproducibility.",
+    )
     default_config = StoryTellerConfig()
     for key, value in dataclasses.asdict(default_config).items():
         parser.add_argument(f"--{key}", type=type(value), default=value)
@@ -32,14 +51,21 @@ def main() -> None:
     set_seed(args.seed)
     set_log_level(logging.WARNING)
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
+    
     config = StoryTellerConfig()
     for field in dataclasses.fields(config):
         name = field.name
         setattr(config, name, getattr(args, name))
+    
     story_teller = StoryTeller(config)
+    
     os.makedirs(args.output_dir, exist_ok=True)
+    
     story_teller.generate(
-        args.writer_prompt, args.painter_prompt_prefix, args.num_images, args.output_dir
+        args.writer_prompt,
+        args.painter_prompt_prefix,
+        args.num_images,
+        args.output_dir
     )
 
 
