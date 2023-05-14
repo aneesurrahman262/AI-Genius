9cf67ae08009bdbc7b51002e36851dace28f51f9
aneesurrahman262 <132897577+aneesurrahman262@users.noreply.github.com>
2023-05-14 23:49:45 +0500

    Update cli.py
    made some chages

--- src/storyteller/cli.py
+++ src/storyteller/cli.py
@@ -13,13 +13,32 @@ def get_args() -> argparse.Namespace:
         "--writer_prompt",
         type=str,
         default="Once upon a time, unicorns roamed the Earth.",
+        help="The prompt for the writer to generate the story.",
     )
     parser.add_argument(
-        "--painter_prompt_prefix", type=str, default="Beautiful painting"
+        "--painter_prompt_prefix",
+        type=str,
+        default="Beautiful painting",
+        help="The prefix for the painter prompt.",
     )
-    parser.add_argument("--num_images", type=int, default=10)
-    parser.add_argument("--output_dir", type=str, default="out")
-    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument(
+        "--num_images",
+        type=int,
+        default=10,
+        help="The number of images to generate.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="out",
+        help="The output directory to save the generated images.",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42,
+        help="The seed value for random number generation.",
+    )
     default_config = StoryTellerConfig()
     for key, value in dataclasses.asdict(default_config).items():
         parser.add_argument(f"--{key}", type=type(value), default=value)
@@ -29,17 +48,34 @@ def get_args() -> argparse.Namespace:
 
 def main() -> None:
     args = get_args()
+
+    # Set the seed for reproducibility
     set_seed(args.seed)
+
+    # Set the log level
     set_log_level(logging.WARNING)
+
+    # Disable tokenizers parallelism
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    # Create the StoryTeller configuration
     config = StoryTellerConfig()
     for field in dataclasses.fields(config):
         name = field.name
         setattr(config, name, getattr(args, name))
+
+    # Create a StoryTeller instance
     story_teller = StoryTeller(config)
+
+    # Create the output directory if it doesn't exist
     os.makedirs(args.output_dir, exist_ok=True)
+
+    # Generate the story
     story_teller.generate(
-        args.writer_prompt, args.painter_prompt_prefix, args.num_images, args.output_dir
+        args.writer_prompt,
+        args.painter_prompt_prefix,
+        args.num_images,
+        args.output_dir,
     )
 
 
