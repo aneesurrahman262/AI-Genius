d1e0458999e8cfcfeeee30345d71c20007da73a9
aneesurrahman262 <132897577+aneesurrahman262@users.noreply.github.com>
2023-05-14 23:52:44 +0500

    Update config.py
    made some changes

--- src/storyteller/config.py
+++ src/storyteller/config.py
@@ -5,9 +5,11 @@ import torch
 
 @dataclass
 class StoryTellerConfig:
-    max_new_tokens: int = 50
-    writer: str = "gpt2"
-    painter: str = "stabilityai/stable-diffusion-2"
-    speaker: str = "tts_models/en/ljspeech/glow-tts"
-    writer_device: str = "cuda:0" if torch.cuda.is_available() else "cpu"
-    painter_device: str = "cuda:0" if torch.cuda.is_available() else "cpu"
+    """Configuration options for the StoryTeller."""
+
+    max_new_tokens: int = 50  # Maximum number of tokens for text generation.
+    writer: str = "gpt2"  # Model used for writing the story.
+    painter: str = "stabilityai/stable-diffusion-2"  # Model used for generating images.
+    speaker: str = "tts_models/en/ljspeech/glow-tts"  # Model used for text-to-speech synthesis.
+    writer_device: str = "cuda:0" if torch.cuda.is_available() else "cpu"  # Device for the writer model.
+    painter_device: str = "cuda:0" if torch.cuda.is_available() else "cpu"  # Device for the painter model.
