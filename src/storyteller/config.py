bea76b51fc955e7c94e104106f74cc75f541b6f3
aneesurrahman262 <132897577+aneesurrahman262@users.noreply.github.com>
2023-05-14 23:57:59 +0500

    Update config.py
    made some changes

--- src/storyteller/config.py
+++ src/storyteller/config.py
@@ -7,7 +7,7 @@ import torch
 class StoryTellerConfig:
     """Configuration options for the StoryTeller."""
 
-    max_new_tokens: int = 50  # Maximum number of tokens for text generation.
+    max_new_tokens: int = 100  # Maximum number of tokens for text generation.
     writer: str = "gpt2"  # Model used for writing the story.
     painter: str = "stabilityai/stable-diffusion-2"  # Model used for generating images.
     speaker: str = "tts_models/en/ljspeech/glow-tts"  # Model used for text-to-speech synthesis.
